aeb3018383507333875687c9a84f3d87b5844ca7
anirudhsinghr <anirudhsinghr@gmail.com>
2016-10-19 22:14:31 +0530

    add livereload and fix additional css bug in gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var compass = require('gulp-compass');
+var connect = require('gulp-connect');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -18,8 +19,8 @@ var sassSources = ['components/sass/style.scss'];
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true })
-			.on('error', gutil.log))
-		.pipe(gulp.dest('components/scripts'))
+		.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'));
 });
 
 gulp.task('js', function() {
@@ -27,6 +28,7 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload());
 });
 
 gulp.task('compass', function() {
@@ -34,10 +36,24 @@ gulp.task('compass', function() {
 		.pipe(compass({
 			sass: 'components/sass',
 			image: 'builds/development/images',
-			style: 'expanded'
+			style: 'expanded',
+			css: 'builds/development/css'
 		}))
 		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload());
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
